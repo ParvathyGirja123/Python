74c48ac31e6831217e02bd20196e5117ffa7eb65
Parvathy <117814878+ParvathyGirja123@users.noreply.github.com>
2022-12-14 10:01:38 +0530

    Update sum of elements in a list.py

--- sum of elements in a list.py
+++ sum of elements in a list.py
@@ -3,3 +3,13 @@ ls=[1,2,3,4,5]
 for i in range(0,len(ls)):
     sum=sum+ls[i]
 print("Sum of elements is:",sum)
+
+..........................OR...................
+ 
+    
+    def sum_list(items):
+        sum_numbers=0
+        for x in items :
+            sum_numbers+=x
+        return sum_numbers
+    print(sum_list[1,2,6])
